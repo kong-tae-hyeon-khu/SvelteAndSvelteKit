5a1a321d742996989212750ba083d210900b3a9d
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-12-05 01:54:06 +0900

    Add Quill Editor

--- src/routes/editor/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Toast from "../../lib/components/Toast.svelte"
-</script>
-<h1>Test</h1>
-<Toast />
- <!-- 다른 컴포넌트나 내용 추가 가능 -->
-  
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Hello
-</h1>
+<script>
+    import { onMount } from "svelte";
+  
+    let editor;
+      
+    let toolbarOptions = [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['image', 'code-block']
+    ]
+      
+
+
+    onMount(async () => {
+        const { default: Quill } = await import("quill");
+      
+        let quill = new Quill(editor, {
+            modules: {
+            toolbar: toolbarOptions
+            },
+            theme: "snow",
+            placeholder: "Write your story..."
+        });
+        quill.on('text-change', onChangeHandler)
+    });
+
+
+    const onChangeHandler = (delta, oldDelta, source) => {
+        const editorRoot = editor.querySelector('.ql-editor');
+        const userHTML = editorRoot.innerHTML;
+        console.log("에디터 내용이 변경되었습니다:", userHTML);
+    }
+  </script>
+  
+  <style>
+    @import 'https://cdn.quilljs.com/1.3.6/quill.snow.css';
+  </style>
+  
+  <div class="editor-wrapper">
+    <div bind:this={editor} on:change={onChangeHandler} />
+  </div>
