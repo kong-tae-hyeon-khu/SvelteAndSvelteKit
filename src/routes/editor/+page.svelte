3d1bd006b5531734f1a2b16d788246d211b7582f
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-12-06 23:55:55 +0900

    Issue : Editor . . .

---
+++ src/routes/editor/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Editor from "cl-editor";
+
+    let html = '<h3>내용을 입력해주세요</h3>'
+
+
+    const onChangeHandler = (evt) => {
+        console.log(evt.detail)
+        console.log(editor)
+        
+    }
+</script>
+
+<main>
+    {@html html}
+    <Editor {html} on:change={onChangeHandler}/>
+</main>
