bdb4e7de5e13539d5662cacb81dd22bb82477f77
공태현 <kong.taehyeon@mintech.kr>
2023-12-22 16:48:13 +0900

    Feat : Hooks

--- study/src/hooks.server.js
+++ study/src/hooks.server.js
@@ -1,7 +1,16 @@
 
 // Before +page.server.js, Run.
-// // Resolve => 넘겨줌.
-// export const handle = ({event, resolve}) => {
-//     console.log(event);
-//     return resolve(event);
-// }
+// Resolve => 넘겨줌.
+export const handle = ({event, resolve}) => {
+    // console.log(event);
+    // return resolve(event);
+
+    if (event.url.pathname === "/user/signin" || event.pathname === "/user/signup") {
+        return resolve(event);
+    }
+
+    else {
+        console.log(event.url.pathname)
+        return resolve(event);
+    }
+}
